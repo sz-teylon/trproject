f0241f946655b51c19f5c4263a26fa64b6e0cc89
sz-teylon <36907349+sz-teylon@users.noreply.github.com>
2022-03-02 13:33:36 +0900

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# nginx ubuntu package ( http://nginx.org/en/linux_packages.html#Ubuntu )
 printf "\nsetup nginx\n"
 sudo apt update && sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
@@ -16,12 +17,14 @@ echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 
     
 sudo apt update && sudo apt install nginx -y
 
+# PPA for php ( https://launchpad.net/~ondrej/+archive/ubuntu/php )
 printf "\nsetup phpx\n"
 sudo apt-get install -y software-properties-common \
   && sudo add-apt-repository ppa:ondrej/php \
   && sudo apt-get update \
   && sudo apt install php7.4-fpm php7.4-mysql php7.4-mbstring php7.4-gd  php7.4-curl php7.4-xml php7.4-bcmath php7.4-zip -y
 
+# copy congiguration
 printf "\nconfig\n"
 sudo mkdir -p /etc/nginx/snippets/ \
  && sudo curl -o /etc/nginx/snippets/fastcgi-php.conf https://raw.githubusercontent.com/sz-teylon/trproject/master/fastcgi-php.conf \
