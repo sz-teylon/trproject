1688597036f8057f00735faf28b2435f2b521c88
sz-teylon <36907349+sz-teylon@users.noreply.github.com>
2022-04-03 18:25:33 +0900

    Update and rename setup1.sh to setup2.sh

--- nginx/setup2.sh
+++ nginx/setup2.sh
@@ -9,8 +9,8 @@ printf "\nsetup nginx\n"
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
   | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
     
-gpg2 --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-printf "\n"
+# gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+# printf "\n"
 
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
 http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
