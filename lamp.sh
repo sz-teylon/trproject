591aab531c41eae5721dd68e30ce35a9a10b2b04
sz-teylon <36907349+sz-teylon@users.noreply.github.com>
2022-05-13 18:18:35 +0900

    Update lamp.sh
    - gnupg2 key check issue - 주석 처리
    - mariadb-server 추가

--- lamp.sh
+++ lamp.sh
@@ -9,8 +9,8 @@ printf "\nsetup nginx\n"
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
   | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
     
-gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-printf "\n"
+# gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+# printf "\n"
 
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
 http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
@@ -22,11 +22,15 @@ echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 
 sudo apt update && sudo apt install nginx -y
 
 # PPA for php ( https://launchpad.net/~ondrej/+archive/ubuntu/php )
-printf "\nsetup phpx\n"
+printf "\nsetup php\n"
 sudo apt-get install -y software-properties-common \
   && sudo add-apt-repository ppa:ondrej/php \
   && sudo apt-get update \
-  && sudo apt install php7.4-fpm php7.4-mysql php7.4-mbstring php7.4-gd  php7.4-curl php7.4-xml php7.4-bcmath php7.4-zip -y
+  && sudo apt install php7.4-fpm php7.4-mysql php7.4-mbstring php7.4-gd php7.4-curl php7.4-xml php7.4-bcmath php7.4-zip -y
+
+# mariadb ( basic repo )
+printf "\nsetup mariadb\n"
+sudo apt install mariadb-server
 
 # copy congiguration
 printf "\nconfig\n"
